044a3683d584c68625628b2e41112b35db3103e8
Daniel Stenberg <daniel@haxx.se>
2019-12-03 11:40:51 +0100

    simplest/mycurlapp.c: global init

--- simplest/mycurlapp.c
+++ simplest/mycurlapp.c
@@ -28,6 +28,8 @@ int main(void)
   CURL *curl;
   CURLcode res;
 
+  curl_global_init(CURL_GLOBAL_ALL);
+
   curl = curl_easy_init();
   if(curl) {
     curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
@@ -42,5 +44,7 @@ int main(void)
     /* always cleanup */
     curl_easy_cleanup(curl);
   }
+
+  curl_global_cleanup();
   return 0;
 }
